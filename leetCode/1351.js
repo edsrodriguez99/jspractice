7d7bc52417292fcc52e44b39abf31e0260075c65
edsrodriguez1999@gmail.com <edsrodriguez1999@gmail.com>
2023-06-08 21:37:38 -0500

    cambios

--- leetCode/1351.js
+++
@@ -1,9 +0,0 @@
-
-
-var countNegatives = function(grid) {
- const flattenedArray = [].concat(...grid);
- const total = flattenedArray.filter (negativo => negativo < 0);
- return total.length
-}
-
-console.log(countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]));
